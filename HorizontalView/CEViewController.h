f04bce646cee27ebe551b88808ac96ba56d0ef17
Chris Eidhof <chris@eidhof.nl>
2012-09-23 20:14:27 +0200

    Initial Commit

---
+++ HorizontalView/CEViewController.h
@@ -0,0 +1,13 @@
+//
+//  CEViewController.h
+//  HorizontalView
+//
+//  Created by Chris Eidhof on 9/23/12.
+//  Copyright (c) 2012 Chris Eidhof. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CEViewController : UIViewController
+
+@end
---
+++ HorizontalView/CEViewController.m
@@ -0,0 +1,29 @@
+//
+//  CEViewController.m
+//  HorizontalView
+//
+//  Created by Chris Eidhof on 9/23/12.
+//  Copyright (c) 2012 Chris Eidhof. All rights reserved.
+//
+
+#import "CEViewController.h"
+
+@interface CEViewController ()
+
+@end
+
+@implementation CEViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
